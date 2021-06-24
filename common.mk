554533f154e1f9f1e893e6e659c7cd2c7569c5c1
sashascurtu <sasha.scurtu@gmail.com>
2021-06-24 20:20:22 +0000

    tama-common: Build mock power stats HAL and label it
    Fixes the following logspam:
    * Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    * BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Change-Id: I721b832f4abf1f31bc5e2a9199cb31f757d9b51b

--- common.mk
+++ common.mk
@@ -378,7 +378,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.sony_sdm845-libperfmgr \
     android.hardware.power@1.3.vendor \
-    libqti-perfd-client
+    libqti-perfd-client \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
