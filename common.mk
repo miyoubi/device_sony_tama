e2b988e92717e9396f4c0e478f7e62129dffed53
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:29:35 +0200

    tama-common: Stop explicitely building netutils-wrapper-1.0
    * Built by default
    
    Change-Id: Id6666535bb8d0b84d2d0a8d19411fafa49df3d68

--- common.mk
+++ common.mk
@@ -325,8 +325,7 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor \
-    netutils-wrapper-1.0
+    android.system.net.netd@1.1.vendor
 
 # Neural networks
 PRODUCT_PACKAGES += \
