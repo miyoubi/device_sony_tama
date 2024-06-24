3218e2fcadf328a4070ba63fbe37a9ccc0e9d1c0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    tama-common: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- common.mk
+++ common.mk
@@ -438,7 +438,9 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget@1.2-service-qti \
+    usb_compositions.conf
 
 # Update engine
 PRODUCT_PACKAGES += \
