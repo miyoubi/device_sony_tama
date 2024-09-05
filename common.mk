1913406bb254405a7f9256b79f4868966f131b67
ekkusa <dayesofficial@gmail.com>
2024-09-06 01:06:07 +0800

    tama-common: Switch to RRO
    * Let's leave SonyOpenTelephony as is
    
    Signed-off-by: ekkusa <dayesofficial@gmail.com>

--- common.mk
+++ common.mk
@@ -21,6 +21,16 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    TamaCarrierConfigOverlay \
+    TamaFrameworksOverlay \
+    TamaSettingsOverlay \
+    TamaSettingsProviderOverlay \
+    TamaSystemUIOverlay \
+    TamaTelephonyOverlay \
+    TetheringConfigOverlay \
+    WifiOverlay
+    
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # A/B
@@ -407,10 +417,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
@@ -438,7 +444,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
