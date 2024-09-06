fa907f661086daefde15b220bd8a66dcdf2d7223
ekkusa <dayesofficial@gmail.com>
2024-09-06 15:02:02 +0800

    tama-common: Allow to build for japanese variants.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "akari akatsuki aurora xz2c"
+write_headers "akari akari_kddi akatsuki akatsuki_kddi aurora aurora_kddi xz2c xz2c_kddi"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
