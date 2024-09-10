334ea350c825e2ef9ad66ca7de845f268d653b13
ekkusa <dayesofficial@gmail.com>
2024-09-10 21:33:32 +0800

    tama-common: Build neuralnetworks@1.3

--- common.mk
+++ common.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
 
 # Neural networks
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.2.vendor
+    android.hardware.neuralnetworks@1.3.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
